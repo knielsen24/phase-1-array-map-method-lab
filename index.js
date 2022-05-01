1fb0c2ea0dbad72e0345413c0303e363f7430f10
Kevin Nielsen <knielsen24@gmail.com>
2022-05-01 09:55:48 -0700

    commit tree 02fbed063e69ed45d1b1668c7ef1eea994e2accd

--- index.js
+++ index.js
@@ -11,6 +11,93 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = () => tutorials.map( (mySentence => mySentence.split(" ").map( (word) => {
+  return word[0].toUpperCase() + word.substring(1)
+  }).join(" ")));
+
+console.log(titleCased())
+
+// const titleCased = array => array.map( (mySentence => mySentence.split(" ").map( (word) => {
+//   return word[0].toUpperCase() + word.substring(1)
+//   }).join(" ")));
+
+// titleCased(tutorials)
+
+//console.log(titleCased(tutorials))
+
+
+// const titleCased = array => array.map( function() {
+//   const words = mySentence.split(" ")
+//   const capFirstLetter = words.map((word) => word[0].toUpperCase() + word.substring(1)).join(" ");
+// })
+
+//console.log(titleCased(tutorials))
+
+// const mySentence = tutorials[0]
+// const capFirstLetter = mySentence.split(" ").map((word) => {
+//   return word[0].toUpperCase() + word.substring(1)
+//   }).join(" ");
+
+// console.log(capFirstLetter)
+
+
+
+
+
+
+////  ******MAPPING ARRAYS CODE ALONG******
+/*
+const oldAccounts = [
+  { userID: 15, title: "Developer Apprentice", accessLevel: "user" },
+  { userID: 63, title: "Developer Apprentice", accessLevel: "user" },
+  { userID: 97, title: "Developer Apprentice", accessLevel: "user" },
+  { userID: 12, title: "Developer Apprentice", accessLevel: "user" },
+  { userID: 44, title: "Developer Apprentice", accessLevel: "user" },
+];
+
+const mapV2 = (array, callback) => {
+  const newArray = []
+  for (const element of array) {
+    newArray.push(callback(element))
+  }
+  return newArray
 }
+
+
+const newEngineers = mapV2(oldAccounts, function (account) {
+  return Object.assign({}, account, { accessLevel: "admin" });
+});
+
+console.log(newEngineers)
+
+const userIDS = mapV2(newEngineers, function (eng) {
+  return eng.userID;
+});
+
+console.log(userIDS)
+
+
+const equippedEngineers = newEngineers.map(eng => {
+  return Object.assign({}, eng, { equipment: "Laptop" })
+})
+
+console.log(equippedEngineers)
+
+
+
+
+const roi = [1000, 25000, 15000, 30000, 8995]
+const initInvest = [10257.69]
+
+
+const mapReturns = array => {
+  const mathArray = []
+  for (const element of array) {
+    let returnFormula = Math.floor(((element-initInvest)/initInvest)* 100)
+    mathArray.push(`Your ROI is ${returnFormula}%`)
+  }
+  return mathArray
+}
+
+console.log(mapReturns(roi))
+*/
